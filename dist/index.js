f10cefb463b867f353f0a2e10ffaaf6618495fa7
vfa-quangpm <138418263+vfa-quangpm@users.noreply.github.com>
2024-08-04 21:56:52 +0700

    npm publish

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-import { jsx as t } from "react/jsx-runtime";
-const r = () => /* @__PURE__ */ t("div", { children: "Button" });
-export {
-  r as Button
-};
--- dist/packages/Atoms/Button/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { FC, ReactNode } from 'react';
-
-export interface IButtonProps {
-    children?: ReactNode;
-}
-declare const Button: FC<IButtonProps>;
-export { Button };
