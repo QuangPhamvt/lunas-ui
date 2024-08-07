a09279c3b2e7f5d3d8d392ed0c3e0bf049d92e3a
Jackesque <tientranquang123@gmail.com>
2024-08-07 21:02:30 +0700

    feat: add wip

--- packages/Atoms/Button/Button.tsx
+++ packages/Atoms/Button/Button.tsx
@@ -5,26 +5,30 @@ import { ButtonHTMLAttributes, forwardRef } from 'react'
 import { Github } from 'lucide-react'
 
 const buttonVariants = cva(
-  'flex items-center justify-center gap-1 rounded-md px-3 py-1 text-base font-semibold text-white enabled:hover:opacity-80 disabled:cursor-not-allowed disabled:opacity-40',
+  'flex items-center justify-center gap-1 rounded-md px-3 py-1 text-base font-semibold text-white enabled:hover:opacity-80 enabled:active:opacity-100 disabled:cursor-not-allowed disabled:opacity-40',
   {
     variants: {
       variant: {
         default:
-          'bg-gradient-to-b from-btn-primary-start to-btn-primary-end enabled:active:opacity-95 enabled:active:shadow-primary',
+          'bg-gradient-to-b from-btn-primary-start to-btn-primary-end enabled:active:opacity-95 enabled:active:shadow-sd-primary',
         secondary:
-          'bg-gradient-to-b from-btn-secondary-start to-btn-secondary-end enabled:active:shadow-base',
+          'bg-gradient-to-b from-btn-secondary-start to-btn-secondary-end enabled:active:shadow-sd-base',
         outline:
-          'border border-solid border-btn-primary-start text-primary enabled:active:shadow-primary',
-        ghost: 'bg-white text-primary enabled:active:shadow-primary',
-        destructive: 'bg-[#FF4858] enabled:active:shadow-destructive',
-        // TODO: add gradient to icon
+          'border border-solid border-btn-primary-start text-primary enabled:active:bg-white enabled:active:shadow-sd-primary',
+        ghost: 'bg-white text-primary enabled:active:shadow-sd-primary',
+        destructive: 'bg-[#FF4858] enabled:active:shadow-sd-destructive',
+        // TODO: add bg color to gradient text
+        // https://stackoverflow.com/questions/74307760/gradient-text-on-button-without-losing-background-color
         'destructive-outline':
-          'border border-solid border-btn-destructive-start bg-[linear-gradient(282deg,var(--tw-gradient-stops))] from-btn-destructive-start from-[7.07%] to-btn-destructive-end to-[92.93%] bg-clip-text text-transparent enabled:active:shadow-destructive',
-        'destructive-ghost': 'bg-white enabled:active:shadow-destructive',
+          'border border-solid border-btn-destructive-start bg-[linear-gradient(282deg,var(--tw-gradient-stops))] from-btn-destructive-start from-[7.07%] to-btn-destructive-end to-[92.93%] bg-clip-text text-transparent enabled:active:bg-white enabled:active:shadow-sd-destructive',
+        'destructive-ghost':
+          'bg-white bg-[linear-gradient(282deg,var(--tw-gradient-stops))] from-btn-destructive-start from-[7.07%] to-btn-destructive-end to-[92.93%] bg-clip-text text-transparent enabled:active:shadow-sd-destructive',
         accept:
-          'bg-[linear-gradient(282deg,var(--tw-gradient-stops))] from-btn-accept-start from-[6.81%] to-btn-accept-end to-[93.19%] enabled:active:shadow-accept',
-        'accept-outline': 'border border-solid border-btn-accept-start enabled:active:shadow-xl',
-        normal: 'border border-solid border-black bg-white text-black enabled:active:shadow-accept',
+          'bg-[linear-gradient(282deg,var(--tw-gradient-stops))] from-btn-accept-start from-[6.81%] to-btn-accept-end to-[93.19%] enabled:active:shadow-sd-accept',
+        'accept-outline':
+          'border border-solid border-btn-accept-start bg-[linear-gradient(282deg,var(--tw-gradient-stops))] from-btn-accept-start from-[6.81%] to-btn-accept-end to-[93.19%] bg-clip-text text-transparent enabled:active:bg-white enabled:active:shadow-sd-destructive enabled:active:shadow-xl',
+        normal:
+          'border border-solid border-black bg-white text-black enabled:active:shadow-sd-accept',
       },
       size: {
         default: '',
@@ -60,7 +64,48 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : 'button'
     return (
       <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
-        {icon && <Github size={16} />}
+        {icon && (
+          <>
+            {['destructive-outline', 'destructive-ghost'].includes(variant!) && (
+              <svg width="0" height="0">
+                <linearGradient
+                  id="icon-gradient"
+                  x1="12.3049"
+                  y1="15.1667"
+                  x2="0.202557"
+                  y2="12.7689"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#FF384A" />
+                  <stop offset="1" stopColor="#FF5463" />
+                </linearGradient>
+              </svg>
+            )}
+            {variant === 'accept-outline' && (
+              <svg width="0" height="0">
+                <linearGradient
+                  id="icon-gradient"
+                  x1="12.3439"
+                  y1="15.1667"
+                  x2="0.179164"
+                  y2="12.7753"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stop-color="#2ED573" />
+                  <stop offset="1" stop-color="#7BED9F" />
+                </linearGradient>
+              </svg>
+            )}
+            <Github
+              size={16}
+              stroke={
+                ['destructive-outline', 'destructive-ghost', 'accept-outline'].includes(variant!)
+                  ? 'url(#icon-gradient)'
+                  : 'currentColor'
+              }
+            />
+          </>
+        )}
         {children}
       </Comp>
     )
