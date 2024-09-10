d810772938026b7b4e05537974d80e802c245f36
vfa-quangpm <138418263+vfa-quangpm@users.noreply.github.com>
2024-09-10 21:14:25 +0700

    feat: #17 add new color

--- packages/tailwindTheme.js
+++ packages/tailwindTheme.js
@@ -63,6 +63,20 @@ export default {
         900: '#3B3C3F',
         950: '#19191B',
       },
+      destructive: {
+        default: '#EF4444',
+        50: '#FEF2F2',
+        100: '#FEE2E2',
+        200: '#FECACA',
+        300: '#FCA5A5',
+        400: '#F87171',
+        500: '#EF4444',
+        600: '#DC2626',
+        700: '#B91C1C',
+        800: '#991B1B',
+        900: '#7F1D1D',
+        950: '#450A0A',
+      },
 
       // TEXT COLOR
       'text-primary': '#111315',
